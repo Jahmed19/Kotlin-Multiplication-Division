dc708ddf2bc80c68b046805004d0f51b193374b1
Jahmed19 <jaria.ahmed@riarauniversity.ac.ke>
2019-11-27 10:41:59 +0300

    Feat
    Feat:Multiplication ,division,functions

--- src/Multiplication.kt
+++ src/Multiplication.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>){
     val first: Int=100
     val second:Int=200
-    val multiply:Int =first*second
+    val 321`multiply:Int =first*second
     println(multiply)
 }
--- src/string/string.kt
+++
@@ -1,5 +0,0 @@
-package string
-
-class string {
-
-}
